fb4999f3e102e77350371b30c96fb4c107ec7f65
DavidDeprost <dadeprost@gmail.com>
2019-08-06 21:49:45 +0200

    Improve control function:
    - Check if the present subtitles are the correct language.
    - If the correct embedded subtitles are present but disabled,
      don't download new subtitles, but enable the embedded ones.

--- autosub.lua
+++ autosub.lua
@@ -1,7 +1,9 @@
 -- Requires Subliminal version 1.0 or newer
 -- Make sure to specify your system's Subliminal location below:
 subliminal = '/opt/anaconda3/bin/subliminal'
-language = 'en'
+-- Specify language in this order: full name, ISO-639-1, ISO-639-2!
+-- (See: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
+language = { 'English', 'en', 'eng' }
 local utils = require 'mp.utils'
 
 -- Log function: log to both terminal and mpv OSD (On-Screen Display)
@@ -16,16 +18,15 @@ function download_subs()
 
     directory, filename = utils.split_path(mp.get_property('path'))
     table = {
-        args = {
-            subliminal, '--debug', 'download', '-s', '-f', '-l', language, '-d', directory, filename
-            -- To see --debug output start mpv from terminal!
+        args = { -- To see --debug output start mpv from terminal!
+            subliminal, '--debug', 'download', '-s', '-f', '-l', language[2], '-d', directory, filename
         }
     }
     result = utils.subprocess(table)
 
     if string.find(result.stdout, 'Downloaded 1 subtitle') then
         -- Subtitles are downloaded successfully, so rescan to activate them:
-        mp.commandv('rescan_external_files') 
+        mp.commandv('rescan_external_files')
         log('Subtitles ready!')
     else
         log('No subtitles were found for this file.', 3)
@@ -39,17 +40,25 @@ function control_download()
         mp.msg.warn('Video is less than 15 minutes\n=> NOT downloading any subtitles')
         return
     end
-    -- There does not seem to be any documentation for the 'sub' property,
-    -- but it works on both internally encoded as well as external subtitle files!
-    -- -> sub = number of subtitle track(s), when they are present
-    -- -> sub = 'no' when subtitles are not present
-    -- -> sub = 'auto' when called before the 'file-loaded' event is triggered
-    sub = tonumber(mp.get_property('sub')) or 0
-    if sub > 0 then
-        mp.msg.warn('Sub track is already present\n=> NOT downloading other subtitles')
-        return
+    track_list = mp.get_property_native('track-list')
+    -- mp.msg.warn('track_list = ', mp.get_property('track-list'), '\n')
+    for i, track in pairs(track_list) do
+        if track['type'] == 'sub' then
+            if track['lang'] == language[2] or track['lang'] == language[3] then
+                mp.msg.warn('Embedded ' .. language[1] .. ' subtitles are already present:\n' ..
+                    '=> NOT downloading new subtitles')
+                if not track['selected'] then
+                    mp.msg.warn('=> Enabling embedded ' .. language[1] .. ' subtitles')
+                    mp.set_property('sid', track['id'])
+                end
+                return
+            elseif track['external'] == true then
+                mp.msg.warn('A matching external subtitle file is present\n=> NOT downloading other subtitles')
+                return
+            end
+        end
     end
-    mp.msg.warn('No sub track was detected\n=> Proceeding to download subtitles:')
+    mp.msg.warn('No ' .. language[1] .. ' subtitles were detected\n=> Proceeding to download:')
     download_subs()
 end
 

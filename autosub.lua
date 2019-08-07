1fd8103605902e07d73adb8e5c6312367cd82b04
DavidDeprost <dadeprost@gmail.com>
2019-08-07 02:43:58 +0200

    Remedy too long line lengths

--- autosub.lua
+++ autosub.lua
@@ -19,7 +19,8 @@ function download_subs()
     directory, filename = utils.split_path(mp.get_property('path'))
     table = {
         args = { -- To see --debug output start mpv from terminal!
-            subliminal, '--debug', 'download', '-s', '-f', '-l', language[2], '-d', directory, filename
+            subliminal, '--debug', 'download', '-s', '-f',
+            '-l', language[2], '-d', directory, filename
         }
     }
     result = utils.subprocess(table)
@@ -29,7 +30,7 @@ function download_subs()
         mp.commandv('rescan_external_files')
         log('Subtitles ready!')
     else
-        log('No subtitles were found for this file.', 3)
+        log('No subtitles were found')
     end
 end
 
@@ -37,28 +38,32 @@ end
 function control_download()
     duration = tonumber(mp.get_property('duration'))
     if duration < 900 then
-        mp.msg.warn('Video is less than 15 minutes\n=> NOT downloading any subtitles')
+        mp.msg.warn('Video is less than 15 minutes\n' ..
+                    '=> NOT downloading any subtitles')
         return
     end
     track_list = mp.get_property_native('track-list')
     -- mp.msg.warn('track_list = ', mp.get_property('track-list'), '\n')
     for i, track in pairs(track_list) do
         if track['type'] == 'sub' then
-            if track['lang'] == language[2] or track['lang'] == language[3] then
-                mp.msg.warn('Embedded ' .. language[1] .. ' subtitles are already present:\n' ..
-                    '=> NOT downloading new subtitles')
+            if track['lang'] == language[3] or track['lang'] == language[2] then
+                mp.msg.warn('Embedded ' .. language[1] ..
+                            ' subtitles are already present:\n' ..
+                            '=> NOT downloading new subtitles')
                 if not track['selected'] then
                     mp.msg.warn('=> Enabling embedded ' .. language[1] .. ' subtitles')
                     mp.set_property('sid', track['id'])
                 end
                 return
             elseif track['external'] == true then
-                mp.msg.warn('A matching external subtitle file is present\n=> NOT downloading other subtitles')
+                mp.msg.warn('A matching external subtitle file is present\n' ..
+                            '=> NOT downloading other subtitles')
                 return
             end
         end
     end
-    mp.msg.warn('No ' .. language[1] .. ' subtitles were detected\n=> Proceeding to download:')
+    mp.msg.warn('No ' .. language[1] .. ' subtitles were detected\n' ..
+                '=> Proceeding to download:')
     download_subs()
 end
 

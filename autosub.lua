c78f09aadf4918917f24423f64b8551b33c91bd7
David Deprost <dadeprost@gmail.com>
2021-01-29 02:08:15 +0100

    Update subliminal path

--- autosub.lua
+++ autosub.lua
@@ -3,7 +3,7 @@
 --=============================================================================
 --          This script uses Subliminal to download subtitles,
 --          so make sure to specify your system's Subliminal location below:
-local subliminal = '/opt/anaconda3/bin/subliminal'
+local subliminal = '/home/david/.local/bin/subliminal'
 --=============================================================================
 -->>    SUBTITLE LANGUAGE:
 --=============================================================================
